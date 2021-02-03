2bad1c4465086c17804d08af9bbb7a84ec9de986
TrinhDang <trinhdang1011@gmail.com>
2021-02-04 00:28:04 +0700

    Giai bai thi nhap mon lap trinh

---
+++ src/Cau1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Cau1 {
+    public static void main(String[] args) {
+        //Tinh va in ra gia tri cua mot bua an
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Moi nhap gia : ");
+        double meal_cost = sc.nextDouble();
+        System.out.println("Moi nhap % tip : ");
+        double tip_percent = sc.nextDouble();
+        System.out.println("Moi nhap % tax : ");
+        double tax_percent = sc.nextDouble();
+        double tip = meal_cost * (tip_percent/100);
+        double tax = meal_cost * (tax_percent/100);
+        double total_cost = meal_cost + tip + tax;
+        System.out.println("Tong tien cua mot bua an la : " + total_cost +" VND");
+    }
+}
---
+++ src/Cau2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Cau2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Moi nhap n: ");
+        int n = sc.nextInt();
+        if (n >= 1 && n <= 100) {
+            if (n % 2 != 0) {
+                System.out.println("Toi la Trinh Van Dang");
+
+            }else if(n >= 2 && n <= 5){
+                System.out.println("Toi khong phai la Trinh Van Dang");
+            }else if (n<= 20){
+                System.out.println("Toi la Trinh Van Dang");
+            }else{
+                System.out.println("Toi khong phai la Trinh Van Dang");
+            }
+        }
+    }
+}
